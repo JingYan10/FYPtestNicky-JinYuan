f1cc3745fc9a756950a8ce3db816795fa1461fe6
ntome19998 <73732118+ntome19998@users.noreply.github.com>
2021-11-06 19:20:50 +0800

    update profile

--- user_profile.php
+++ user_profile.php
@@ -84,61 +84,13 @@ if ($resultCheck > 0) {
     </div>
 </div>
 
-<div class="seller-container">
-    <div class="product-container">
-        <div class="productInfo">
-            <a href="createProduct.php"><input type="button" class="btnCreateProduct" value="createProduct"></a>
-            <button class="btnShowProductListing" onclick="toggleProductListing()">show product listing</button>
-            <div id="product-listing" style="display:none;" class="product-listing">
-
-                
-                        <?php
-                        $sql = "SELECT * FROM product where userEmail='tete1234@gmail.com' AND deleteStatus IS NULL ";
-                        $result = mysqli_query($conn, $sql);
-                        $resultCheck = mysqli_num_rows($result);
-                       
-                        if ($resultCheck > 0) {
-                            echo '<table>
-                            <caption>Products</caption>
-                            <thead>
-                                <tr>
-                                    <th scope="col">Product ID</th>
-                                    <th scope="col">Product name</th>
-                                    <th scope="col">product image</th>
-                                    <th scope="col">product quantity</th>
-                                    <th scope="col">product price</th>
-                                    <th scope="col"></th>
-                                </tr>
-                            </thead><tbody>';
-                            while ($row = mysqli_fetch_assoc($result)) {
-                                echo "<tr>";
-                                echo "<td>" . "P00" . $row['productID'] . "</td>";
-                                echo "<td>" . $row['productName'] . "</td>";
-                                echo "<td>" . "<img style='height:140px;width:140px;' src=" . $row['productImage'] . ">" . "</td>";
-                                echo "<td>" . $row['productQuantity'] . "</td>";
-                                echo "<td>" . $row['productPrice'] . "</td>";
-                                echo "<td>";
-                                $productData = "productID=" . $row['productID'] . "&productName=" . $row['productName'] . "&productImage=" . $row['productImage'] . "&productQuantity=" . $row['productQuantity'] . "&productPrice=" . $row['productPrice'];
-                                echo "<a href='editProduct.php?" . $productData . "'>" . "<button class='btnEditProduct'>edit</button></a>";
-                                echo "<a href='deleteProduct.php?".$productData."'>"."<button class='btnDeleteProduct'>delete</button></a>";
-                                echo "</td>";
-                                echo "</tr>";
-                            }
-                            echo ' </tbody></table>';
-                        } else {
-                            echo 'there is no product created';
-                        }
-                        ?>
-                <div style="margin-bottom:50px;"></div>
-
-                
+<?php
+    if($_SESSION["userRole"]=="seller"){
+        include_once 'sellerContainer.php';
+    }
+?>
 
 
-
-            </div>
-        </div>
-    </div>
-</div>
 
 <script>
     function toggleProductListing() {
